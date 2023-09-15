e4cf18a31380590d8e9538a84caada4fc4f699b1
Carlator <clorenzozuniga@gmail.com>
2023-09-15 20:35:37 +0200

    - start villain arc button (message is joke)

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+
     const accent = { color: '#b12f2f' };
     const primary = { color: '#0a6161' };
+    let navigate = useNavigate();
+
+    function executeOrder66 () {
+        navigate("/profile");
+    }
 
     return (
         <div id='home'>
@@ -12,7 +19,7 @@ export default function Home() {
                     A <span style={primary}>GODLIKE</span> physique can only be achieved through a rigorous diet. <br />
                     So start <span style={primary}>CUTTING</span> you <span style={accent}>FAT FUCK</span> and <span style={primary}>BULKING</span> you <span style={accent}>SKINNY BITCH</span>.
                 </h2>
-                <button id="villain-arc" className='border'><h3>START VILLAIN ARC</h3></button>
+                <button onClick={executeOrder66} id="villain-arc" className='border'><h3>START VILLAIN ARC</h3></button>
             </span>
             
         </div>
